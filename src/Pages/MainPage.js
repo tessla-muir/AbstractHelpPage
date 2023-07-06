c0029d9724d1c45a3dffa262fac63b45df179154
Tessla Muir <tesslamuir@yahoo.com>
2023-07-06 00:27:54 +0000

    Card component w props

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,8 +1,18 @@
 import styles from "./MainPage.module.css";
 import SearchHelp from "../Components/SearchHelp.js";
+import Card from "../Components/Card.js";
 
 function MainPage() {
-  return <SearchHelp />;
+  return (
+    <div>
+      <SearchHelp />
+      <Card
+        title="Billing"
+        description="Access payment options."
+        link="https://tessla-muir.com/about"
+      />
+    </div>
+  );
 }
 
 export default MainPage;
